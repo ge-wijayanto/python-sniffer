2ade5da503b3598761ed196de62ff1cdd19508c6
ge-wijayanto <greg.evan01@gmail.com>
2023-03-21 09:33:00 +0700

    try solve

--- core-functions/sniff-func.py
+++ core-functions/sniff-func.py
@@ -52,18 +52,19 @@ def sniffStart():
 
                 # IP Header Capture
                 ip_header = captured_packet[0][14:34]
-                ip = struct.unpack('!BBHHHBBH4s4s', ip_header)
+                ip = struct.unpack('!BBBHHHBBH4s4s', ip_header)
                 print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}IP Header:{Style.RESET_ALL}')
-                print(f'     - IP Header Length (IHL)\t: {Fore.GREEN}{ip[0]}{Style.RESET_ALL}')
-                # print(f'     - Type of Service (TOS)\t: {Fore.GREEN}{ip[1].decode("utf-8")}{Style.RESET_ALL}')
-                # print(f'     - Total Length\t: {Fore.GREEN}{ip[2]}{Style.RESET_ALL}')
-                # print(f'     - Identification\t: {Fore.GREEN}{ip[3]}{Style.RESET_ALL}')
-                # print(f'     - Fragment Offset\t: {Fore.GREEN}{ip[4]}{Style.RESET_ALL}')
-                # print(f'     - Time-to-Live (TTL)\t: {Fore.GREEN}{ip[5].decode("utf-8")}{Style.RESET_ALL}')
-                # print(f'     - Protocol\t\t: {Fore.GREEN}{ip[6].decode("utf-8")}{Style.RESET_ALL}')
-                # print(f'     - Header Checksum\t: {Fore.GREEN}{ip[7]}{Style.RESET_ALL}')
-                print(f'     - Source IP \t: {Fore.GREEN}{socket.inet_ntoa(ip[8])}{Style.RESET_ALL}')
-                print(f'     - Destination IP \t: {Fore.GREEN}{socket.inet_ntoa(ip[9])}{Style.RESET_ALL}')
+                print(f'     - IP Version\t: {Fore.GREEN}{ip[0]}{Style.RESET_ALL}')
+                print(f'     - IP Header Length (IHL)\t: {Fore.GREEN}{ip[1]}{Style.RESET_ALL}')
+                print(f'     - Type of Service (TOS)\t: {Fore.GREEN}{ip[2]}{Style.RESET_ALL}')
+                print(f'     - Total Length\t: {Fore.GREEN}{ip[3]}{Style.RESET_ALL}')
+                print(f'     - Identification\t: {Fore.GREEN}{ip[4]}{Style.RESET_ALL}')
+                print(f'     - Fragment Offset\t: {Fore.GREEN}{ip[5]}{Style.RESET_ALL}')
+                print(f'     - Time-to-Live (TTL)\t: {Fore.GREEN}{ip[6]}{Style.RESET_ALL}')
+                print(f'     - Protocol\t\t: {Fore.GREEN}{ip[7]}{Style.RESET_ALL}')
+                print(f'     - Header Checksum\t: {Fore.GREEN}{ip[8]}{Style.RESET_ALL}')
+                print(f'     - Source IP \t: {Fore.GREEN}{socket.inet_ntoa(ip[9])}{Style.RESET_ALL}')
+                print(f'     - Destination IP \t: {Fore.GREEN}{socket.inet_ntoa(ip[10])}{Style.RESET_ALL}')
                 
                 end = time.time()
                 
