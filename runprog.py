8c998d7918f3f20bf867e25e672a81031604cbe5
ge-wijayanto <greg.evan01@gmail.com>
2023-03-26 20:40:07 +0700

    Edit args name

--- runprog.py
+++ runprog.py
@@ -9,13 +9,13 @@ import core_functions.sniff_func as sniff_func
 
 cmd_parser = argparse.ArgumentParser(description='py-sniff - A Python Packet Sniffer', formatter_class=argparse.RawTextHelpFormatter)
 
-cmd_parser.add_argument('-s', '--scan', action='store_true', help='Scan for available networks')
+cmd_parser.add_argument('-nS', '--netscan', action='store_true', help='Scan for available networks')
 cmd_parser.add_argument('-c', '--connect', action='store_true', help='Connect to a network')
-cmd_parser.add_argument('-S', '--sniff', action='store_true', help='Start packet sniffing on network')
+cmd_parser.add_argument('-s', '--sniff', action='store_true', help='Start packet sniffing on network')
 
 args = vars(cmd_parser.parse_args())
 
-if args['scan']:
+if args['netscan']:
     network_scan.main()
     exit(0)
 elif args['connect']:
