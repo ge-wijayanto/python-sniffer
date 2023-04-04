61314a099a065b667ae087bf853d1146b26e777f
ge-wijayanto <greg.evan01@gmail.com>
2023-04-04 12:13:47 +0700

    function refactoring

--- core_functions/sniff_func.py
+++ core_functions/sniff_func.py
@@ -46,74 +46,26 @@ def sniffStart(log):
                 # Ethernet Header Capture
                 eth_header = captured_packet[0][0:14]
                 eth = struct.unpack('!6s6s2s', eth_header)
-                print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}Ethernet Header:{Style.RESET_ALL}')
-                print(f'     - Destination MAC\t\t: {Fore.GREEN}{binascii.hexlify(eth[0]).decode("utf-8").upper()}{Style.RESET_ALL}')
-                print(f'     - Source MAC\t\t: {Fore.GREEN}{binascii.hexlify(eth[1]).decode("utf-8").upper()}{Style.RESET_ALL}')
-                print(f'     - Type/Length\t\t: {Fore.GREEN}{binascii.hexlify(eth[2]).decode("utf-8")}{Style.RESET_ALL}')
+                getEthernetHeader(eth)
 
                 # IP Header Capture
                 ip_header = captured_packet[0][14:34]
                 ip = struct.unpack('!BBHHHBBH4s4s', ip_header)
-                print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}IP Header:{Style.RESET_ALL}')
-                
-                split_version_IHL = BitArray(hex(ip[0]))
-                
-                print(f'     - IP Version\t\t: {Fore.GREEN}{split_version_IHL.bin[:4]} ({int(split_version_IHL.bin[:4], 2)}){Style.RESET_ALL}')
-                print(f'     - IP Header Length (IHL)\t: {Fore.GREEN}{split_version_IHL.bin[4:]} ({int(split_version_IHL.bin[4:], 2)*4} bytes ({int(split_version_IHL.bin[4:], 2)})){Style.RESET_ALL}')
-                print(f'     - Type of Service (TOS)\t: {Fore.GREEN}{ip[1]}{Style.RESET_ALL}')
-                print(f'     - Total Length\t\t: {Fore.GREEN}{ip[2]}{Style.RESET_ALL}')
-                print(f'     - Identification\t\t: {Fore.GREEN}{ip[3]}{Style.RESET_ALL}')
-                print(f'     - Fragment Offset\t\t: {Fore.GREEN}{ip[4]}{Style.RESET_ALL}')
-                print(f'     - Time-to-Live (TTL)\t: {Fore.GREEN}{ip[5]}{Style.RESET_ALL}')
-                print(f'     - Protocol\t\t\t: {Fore.GREEN}{ip[6]}{Style.RESET_ALL}')
-                print(f'     - Header Checksum\t\t: {Fore.GREEN}{ip[7]}{Style.RESET_ALL}')
-                print(f'     - Source IP\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[8])}{Style.RESET_ALL}')
-                print(f'     - Destination IP\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[9])}{Style.RESET_ALL}')
+                getIPHeader(ip)
                 
                 ## Transport Header
                 if ip[6] == 6:
                     tcp_header = captured_packet[0][34:54]
                     tcp = struct.unpack('!HHLLBBHHH', tcp_header)
-                    print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}TCP Header:{Style.RESET_ALL}')
-                    print(f'     - Source Port\t\t: {Fore.GREEN}{tcp[0]}{Style.RESET_ALL}')
-                    print(f'     - Destination Port\t\t: {Fore.GREEN}{tcp[1]}{Style.RESET_ALL}')
-                    print(f'     - Sequence Number\t\t: {Fore.GREEN}{tcp[2]}{Style.RESET_ALL}')
-                    print(f'     - Acknowledgement Number\t: {Fore.GREEN}{tcp[3]}{Style.RESET_ALL}')
-                    
-                    split_version_TCP = BitArray(hex(tcp[4]))
-                    split_version_flags = BitArray(hex(tcp[5]))
-                    print(f'     - Header Length\t\t: {Fore.GREEN}{split_version_TCP.bin[:4]} ({int(split_version_TCP.bin[:4], 2)*4} bytes ({int(split_version_TCP.bin[:4], 2)}){Style.RESET_ALL}')
-                    print(f'     - Flags\t\t\t: {Fore.GREEN}{tcp[5]}{Style.RESET_ALL}')
-                    print(f'          - Reserved\t\t: {Fore.GREEN}{split_version_TCP.bin[4:7]}{Style.RESET_ALL}')
-                    print(f'          - Accurate ECN\t\t: {Fore.GREEN}{split_version_TCP.bin[7]}{Style.RESET_ALL}')
-                    print(f'          - Congestion Window\t: {Fore.GREEN}{split_version_flags.bin[0]}{Style.RESET_ALL}')
-                    print(f'          - ECN-Echo\t\t: {Fore.GREEN}{split_version_flags.bin[1]}{Style.RESET_ALL}')
-                    print(f'          - URG\t\t\t: {Fore.GREEN}{split_version_flags.bin[2]}{Style.RESET_ALL}')
-                    print(f'          - ACK\t\t\t: {Fore.GREEN}{split_version_flags.bin[3]}{Style.RESET_ALL}')
-                    print(f'          - PSH\t\t\t: {Fore.GREEN}{split_version_flags.bin[4]}{Style.RESET_ALL}')
-                    print(f'          - RST\t\t\t: {Fore.GREEN}{split_version_flags.bin[5]}{Style.RESET_ALL}')
-                    print(f'          - SYN\t\t\t: {Fore.GREEN}{split_version_flags.bin[6]}{Style.RESET_ALL}')
-                    print(f'          - FIN\t\t\t: {Fore.GREEN}{split_version_flags.bin[7]}{Style.RESET_ALL}')
-                    print(f'     - Window Size\t\t: {Fore.GREEN}{tcp[6]}{Style.RESET_ALL}')
-                    print(f'     - Checksum\t\t\t: {Fore.GREEN}{tcp[7]}{Style.RESET_ALL}')
-                    print(f'     - Urgent Pointer\t\t: {Fore.GREEN}{tcp[8]}{Style.RESET_ALL}')
+                    getTCPHeader(tcp)
                 elif ip[6] == 17:
                     udp_header = captured_packet[0][34:42]
                     udp = struct.unpack('!HHHH', udp_header)
-                    print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}UDP Header:{Style.RESET_ALL}')
-                    print(f'     - Source Port\t\t: {Fore.GREEN}{udp[0]}{Style.RESET_ALL}')
-                    print(f'     - Destination Port\t\t: {Fore.GREEN}{udp[1]}{Style.RESET_ALL}')
-                    print(f'     - Length\t\t: {Fore.GREEN}{udp[2]}{Style.RESET_ALL}')
-                    print(f'     - Checksum\t\t: {Fore.GREEN}{udp[3]}{Style.RESET_ALL}')
+                    getUDPHeader(udp)
                 elif ip[6] == 1:
                     icmp_header = captured_packet[0][34:42]
                     icmp = struct.unpack('!BBHHH', icmp_header)
-                    print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}ICMP Header:{Style.RESET_ALL}')
-                    print(f'     - Type\t\t\t: {Fore.GREEN}{icmp[0]}{Style.RESET_ALL}')
-                    print(f'     - Code\t\t\t: {Fore.GREEN}{icmp[1]}{Style.RESET_ALL}')
-                    print(f'     - Checksum\t\t\t: {Fore.GREEN}{icmp[2]}{Style.RESET_ALL}')
-                    print(f'     - Identifier\t\t: {Fore.GREEN}{icmp[3]}{Style.RESET_ALL}')
-                    print(f'     - Sequence Number\t\t: {Fore.GREEN}{icmp[4]}{Style.RESET_ALL}')
+                    getICMPHeader(icmp)
                 else:
                     continue
                     
@@ -151,6 +103,69 @@ def sniffStart(log):
             except Exception as e:
                 log.close()
                 print(f'[{Fore.RED}!{Style.RESET_ALL}] ERROR\t\t: {Fore.RED}{e}{Style.RESET_ALL}')
+
+def getEthernetHeader(eth):
+    print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}Ethernet Header:{Style.RESET_ALL}')
+    print(f'     - Destination MAC\t\t: {Fore.GREEN}{binascii.hexlify(eth[0]).decode("utf-8").upper()}{Style.RESET_ALL}')
+    print(f'     - Source MAC\t\t: {Fore.GREEN}{binascii.hexlify(eth[1]).decode("utf-8").upper()}{Style.RESET_ALL}')
+    print(f'     - Type/Length\t\t: {Fore.GREEN}{binascii.hexlify(eth[2]).decode("utf-8")}{Style.RESET_ALL}')
+    
+def getIPHeader(ip):
+    print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}IP Header:{Style.RESET_ALL}')
+                
+    split_version_IHL = BitArray(hex(ip[0]))
+    
+    print(f'     - IP Version\t\t: {Fore.GREEN}{split_version_IHL.bin[:4]} ({int(split_version_IHL.bin[:4], 2)}){Style.RESET_ALL}')
+    print(f'     - IP Header Length (IHL)\t: {Fore.GREEN}{split_version_IHL.bin[4:]} ({int(split_version_IHL.bin[4:], 2)*4} bytes ({int(split_version_IHL.bin[4:], 2)})){Style.RESET_ALL}')
+    print(f'     - Type of Service (TOS)\t: {Fore.GREEN}{ip[1]}{Style.RESET_ALL}')
+    print(f'     - Total Length\t\t: {Fore.GREEN}{ip[2]}{Style.RESET_ALL}')
+    print(f'     - Identification\t\t: {Fore.GREEN}{ip[3]}{Style.RESET_ALL}')
+    print(f'     - Fragment Offset\t\t: {Fore.GREEN}{ip[4]}{Style.RESET_ALL}')
+    print(f'     - Time-to-Live (TTL)\t: {Fore.GREEN}{ip[5]}{Style.RESET_ALL}')
+    print(f'     - Protocol\t\t\t: {Fore.GREEN}{ip[6]}{Style.RESET_ALL}')
+    print(f'     - Header Checksum\t\t: {Fore.GREEN}{ip[7]}{Style.RESET_ALL}')
+    print(f'     - Source IP\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[8])}{Style.RESET_ALL}')
+    print(f'     - Destination IP\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[9])}{Style.RESET_ALL}')
+
+def getTCPHeader(tcp):
+    print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}TCP Header:{Style.RESET_ALL}')
+    print(f'     - Source Port\t\t: {Fore.GREEN}{tcp[0]}{Style.RESET_ALL}')
+    print(f'     - Destination Port\t\t: {Fore.GREEN}{tcp[1]}{Style.RESET_ALL}')
+    print(f'     - Sequence Number\t\t: {Fore.GREEN}{tcp[2]}{Style.RESET_ALL}')
+    print(f'     - Acknowledgement Number\t: {Fore.GREEN}{tcp[3]}{Style.RESET_ALL}')
+    
+    split_version_TCP = BitArray(hex(tcp[4]))
+    split_version_flags = BitArray(hex(tcp[5]))
+    print(f'     - Header Length\t\t: {Fore.GREEN}{split_version_TCP.bin[:4]} ({int(split_version_TCP.bin[:4], 2)*4} bytes ({int(split_version_TCP.bin[:4], 2)}){Style.RESET_ALL}')
+    print(f'     - Flags\t\t\t: {Fore.GREEN}{tcp[5]}{Style.RESET_ALL}')
+    print(f'          - Reserved\t\t: {Fore.GREEN}{split_version_TCP.bin[4:7]}{Style.RESET_ALL}')
+    print(f'          - Accurate ECN\t\t: {Fore.GREEN}{split_version_TCP.bin[7]}{Style.RESET_ALL}')
+    print(f'          - Congestion Window\t: {Fore.GREEN}{split_version_flags.bin[0]}{Style.RESET_ALL}')
+    print(f'          - ECN-Echo\t\t: {Fore.GREEN}{split_version_flags.bin[1]}{Style.RESET_ALL}')
+    print(f'          - URG\t\t\t: {Fore.GREEN}{split_version_flags.bin[2]}{Style.RESET_ALL}')
+    print(f'          - ACK\t\t\t: {Fore.GREEN}{split_version_flags.bin[3]}{Style.RESET_ALL}')
+    print(f'          - PSH\t\t\t: {Fore.GREEN}{split_version_flags.bin[4]}{Style.RESET_ALL}')
+    print(f'          - RST\t\t\t: {Fore.GREEN}{split_version_flags.bin[5]}{Style.RESET_ALL}')
+    print(f'          - SYN\t\t\t: {Fore.GREEN}{split_version_flags.bin[6]}{Style.RESET_ALL}')
+    print(f'          - FIN\t\t\t: {Fore.GREEN}{split_version_flags.bin[7]}{Style.RESET_ALL}')
+    print(f'     - Window Size\t\t: {Fore.GREEN}{tcp[6]}{Style.RESET_ALL}')
+    print(f'     - Checksum\t\t\t: {Fore.GREEN}{tcp[7]}{Style.RESET_ALL}')
+    print(f'     - Urgent Pointer\t\t: {Fore.GREEN}{tcp[8]}{Style.RESET_ALL}')
+                    
+def getUDPHeader(udp):
+    print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}UDP Header:{Style.RESET_ALL}')
+    print(f'     - Source Port\t\t: {Fore.GREEN}{udp[0]}{Style.RESET_ALL}')
+    print(f'     - Destination Port\t\t: {Fore.GREEN}{udp[1]}{Style.RESET_ALL}')
+    print(f'     - Length\t\t: {Fore.GREEN}{udp[2]}{Style.RESET_ALL}')
+    print(f'     - Checksum\t\t: {Fore.GREEN}{udp[3]}{Style.RESET_ALL}')
+    
+def getICMPHeader(icmp):
+    print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}ICMP Header:{Style.RESET_ALL}')
+    print(f'     - Type\t\t\t: {Fore.GREEN}{icmp[0]}{Style.RESET_ALL}')
+    print(f'     - Code\t\t\t: {Fore.GREEN}{icmp[1]}{Style.RESET_ALL}')
+    print(f'     - Checksum\t\t\t: {Fore.GREEN}{icmp[2]}{Style.RESET_ALL}')
+    print(f'     - Identifier\t\t: {Fore.GREEN}{icmp[3]}{Style.RESET_ALL}')
+    print(f'     - Sequence Number\t\t: {Fore.GREEN}{icmp[4]}{Style.RESET_ALL}')
 
 def main():
     timestamp = time.strftime('%a, %d %b %Y %H:%M', time.localtime())
