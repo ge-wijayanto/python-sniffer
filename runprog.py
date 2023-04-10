7e50232c581a297ebb17aa7445098ea1e06d16a6
ge-wijayanto <greg.evan01@gmail.com>
2023-04-10 14:03:10 +0700

    Fix error from refactoring

--- runprog.py
+++ runprog.py
@@ -2,8 +2,6 @@ import argparse
 
 import core_functions.network_scan as network_scan
 import core_functions.connect as connect
-import core_functions.sniff_all as sniff_all
-import core_functions.sniff_port as sniff_port
 import core_functions.sniff_func as sniff_func
 
 cmd_parser = argparse.ArgumentParser(description='py-sniff - A Python Packet Sniffer', formatter_class=argparse.RawTextHelpFormatter)
