b82f7ea063368ad60ad79e4120059bbce9681a76
ge-wijayanto <greg.evan01@gmail.com>
2023-04-10 15:06:25 +0700

    Simplify Console Log Output

--- additional_functions/console_logger.py
+++ additional_functions/console_logger.py
@@ -10,7 +10,7 @@ def getEthernetHeader(eth):
     # print(f'     - Destination MAC\t\t: {Fore.GREEN}{binascii.hexlify(eth[0]).decode("utf-8").upper()}{Style.RESET_ALL}')
     # print(f'     - Source MAC\t\t: {Fore.GREEN}{binascii.hexlify(eth[1]).decode("utf-8").upper()}{Style.RESET_ALL}')
     # print(f'     - Type/Length\t\t: {Fore.GREEN}{binascii.hexlify(eth[2]).decode("utf-8")}{Style.RESET_ALL}')
-    print(f'     Src. MAC : {Fore.GREEN}{0}{Style.RESET_ALL}, Dest. MAC : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
+    print('     Src. MAC : {Fore.GREEN}{0}{Style.RESET_ALL}, Dest. MAC : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
         binascii.hexlify(eth[1]).decode("utf-8").upper(), binascii.hexlify(eth[0]).decode("utf-8").upper()
         ))
     
@@ -30,10 +30,10 @@ def getIPHeader(ip):
     # print(f'     - Header Checksum\t\t: {Fore.GREEN}{hex(ip[7])}{Style.RESET_ALL}')
     # print(f'     - Source IP\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[8])}{Style.RESET_ALL}')
     # print(f'     - Destination IP\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[9])}{Style.RESET_ALL}')
-    print(f'     IP Version : {Fore.GREEN}{0} ({1}){Style.RESET_ALL}, Protocol : {Fore.GREEN}{2}{Style.RESET_ALL}'.format(
+    print('     IP Version : {Fore.GREEN}{0} ({1}){Style.RESET_ALL}, Protocol : {Fore.GREEN}{2}{Style.RESET_ALL}'.format(
         split_version_IHL.bin[:4], int(split_version_IHL.bin[:4], 2), proto=ip[6]
         ))
-    print(f'     Src. IP : {Fore.GREEN}{0}{Style.RESET_ALL}, Dest. IP : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
+    print('     Src. IP : {Fore.GREEN}{0}{Style.RESET_ALL}, Dest. IP : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
         socket.inet_ntoa(ip[8]), socket.inet_ntoa(ip[9])
         ))
     
@@ -60,13 +60,13 @@ def getTCPHeader(tcp):
     # print(f'     - Window Size\t\t: {Fore.GREEN}{tcp[6]}{Style.RESET_ALL}')
     # print(f'     - Checksum\t\t\t: {Fore.GREEN}{hex(tcp[7])}{Style.RESET_ALL}')
     # print(f'     - Urgent Pointer\t\t: {Fore.GREEN}{tcp[8]}{Style.RESET_ALL}')
-    print(f'     Src. Port : {Fore.GREEN}{0}{Style.RESET_ALL}, Dest. Port : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
+    print('     Src. Port : {Fore.GREEN}{0}{Style.RESET_ALL}, Dest. Port : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
         tcp[0], tcp[1]
     ))
-    print(f'     Flags : {Fore.GREEN}{split_HL_flags[4:] + split_flags[0:]}{Style.RESET_ALL}')
-    print(f'       URG : {Fore.GREEN}{0}{Style.RESET_ALL}, ACK : {Fore.GREEN}{1}{Style.RESET_ALL}, PSH : {Fore.GREEN}{2}{Style.RESET_ALL}'
+    print('     Flags : {Fore.GREEN}{split_HL_flags[4:] + split_flags[0:]}{Style.RESET_ALL}')
+    print('       URG : {Fore.GREEN}{0}{Style.RESET_ALL}, ACK : {Fore.GREEN}{1}{Style.RESET_ALL}, PSH : {Fore.GREEN}{2}{Style.RESET_ALL}'
           .format(split_flags.bin[2], split_flags.bin[3], split_flags.bin[4]))
-    print(f'       RST : {Fore.GREEN}{0}{Style.RESET_ALL}, SYN : {Fore.GREEN}{1}{Style.RESET_ALL}, FIN : {Fore.GREEN}{2}{Style.RESET_ALL}'
+    print('       RST : {Fore.GREEN}{0}{Style.RESET_ALL}, SYN : {Fore.GREEN}{1}{Style.RESET_ALL}, FIN : {Fore.GREEN}{2}{Style.RESET_ALL}'
           .format(split_flags.bin[5], split_flags.bin[6], split_flags.bin[7]))
         
 def getUDPHeader(udp):
@@ -75,7 +75,7 @@ def getUDPHeader(udp):
     # print(f'     - Destination Port\t\t: {Fore.GREEN}{udp[1]}{Style.RESET_ALL}')
     # print(f'     - Length\t\t\t: {Fore.GREEN}{udp[2]}{Style.RESET_ALL}')
     # print(f'     - Checksum\t\t\t: {Fore.GREEN}{hex(udp[3])}{Style.RESET_ALL}')
-    print(f'     Src. Port : {Fore.GREEN}{0}{Style.RESET_ALL}, Dest. Port : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
+    print('     Src. Port : {Fore.GREEN}{0}{Style.RESET_ALL}, Dest. Port : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
         udp[0], udp[1]
     ))
 
@@ -86,10 +86,10 @@ def getICMPHeader(icmp):
     # print(f'     - Checksum\t\t\t: {Fore.GREEN}{hex(icmp[2])}{Style.RESET_ALL}')
     # print(f'     - Identifier\t\t: {Fore.GREEN}{icmp[3]}{Style.RESET_ALL}')
     # print(f'     - Sequence Number\t\t: {Fore.GREEN}{icmp[4]}{Style.RESET_ALL}')
-    print(f'     Type : {Fore.GREEN}{0}{Style.RESET_ALL}, Code : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
+    print('     Type : {Fore.GREEN}{0}{Style.RESET_ALL}, Code : {Fore.GREEN}{1}{Style.RESET_ALL}'.format(
         icmp[0], icmp[1]  
     ))
-    print(f'     Identifier\t\t: {Fore.GREEN}{icmp[3]}{Style.RESET_ALL}')
+    print(f'     Identifier\t: {Fore.GREEN}{icmp[3]}{Style.RESET_ALL}')
 
 # def getData(data):
 #     print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}Data:{Style.RESET_ALL}')
