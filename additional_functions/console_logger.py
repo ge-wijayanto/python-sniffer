772f732b86d2ef97cba97528e7584e5dbef54ba6
ge-wijayanto <greg.evan01@gmail.com>
2023-04-10 15:19:52 +0700

    Simplify Console Log Output

--- additional_functions/console_logger.py
+++ additional_functions/console_logger.py
@@ -7,23 +7,23 @@ def getEthernetHeader(eth):
     print('---------------------------------------------------------------------')
     
     print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}Ethernet Header:{Style.RESET_ALL}')
-    print(f'     - Src. MAC\t\t\t: {Fore.GREEN}{binascii.hexlify(eth[1]).decode("utf-8").upper()}{Style.RESET_ALL}')
-    print(f'     - Dest. MAC\t\t\t: {Fore.GREEN}{binascii.hexlify(eth[0]).decode("utf-8").upper()}{Style.RESET_ALL}')
+    print(f'     - Src. MAC\t\t: {Fore.GREEN}{binascii.hexlify(eth[1]).decode("utf-8").upper()}{Style.RESET_ALL}')
+    print(f'     - Dest. MAC\t: {Fore.GREEN}{binascii.hexlify(eth[0]).decode("utf-8").upper()}{Style.RESET_ALL}')
     
 def getIPHeader(ip):
     split_version_IHL = BitArray(hex(ip[0]))
     print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}IP Header:{Style.RESET_ALL}')
-    print(f'     - IP Version\t\t: {Fore.GREEN}{split_version_IHL.bin[:4]} ({int(split_version_IHL.bin[:4], 2)}){Style.RESET_ALL}')
-    print(f'     - Protocol\t\t\t: {Fore.GREEN}{ip[6]}{Style.RESET_ALL}')
-    print(f'     - Src. IP\t\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[8])}{Style.RESET_ALL}')
-    print(f'     - Dest. IP\t\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[9])}{Style.RESET_ALL}')
+    print(f'     - IP Version\t: {Fore.GREEN}{split_version_IHL.bin[:4]} ({int(split_version_IHL.bin[:4], 2)}){Style.RESET_ALL}')
+    print(f'     - Protocol\t\t: {Fore.GREEN}{ip[6]}{Style.RESET_ALL}')
+    print(f'     - Src. IP\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[8])}{Style.RESET_ALL}')
+    print(f'     - Dest. IP\t\t: {Fore.GREEN}{socket.inet_ntoa(ip[9])}{Style.RESET_ALL}')
     
 def getTCPHeader(tcp):
     split_flags = BitArray(hex(tcp[5]))
     print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}TCP Header:{Style.RESET_ALL}')
-    print(f'     - Src. Port\t\t\t: {Fore.GREEN}{tcp[0]}{Style.RESET_ALL}')
-    print(f'     - Dest. Port\t\t: {Fore.GREEN}{tcp[1]}{Style.RESET_ALL}')
-    print(f'     - Flags\t\t\t: {Fore.GREEN}{split_HL_flags[4:] + split_flags[0:]}{Style.RESET_ALL}')
+    print(f'     - Src. Port\t\t: {Fore.GREEN}{tcp[0]}{Style.RESET_ALL}')
+    print(f'     - Dest. Port\t: {Fore.GREEN}{tcp[1]}{Style.RESET_ALL}')
+    print(f'     - Flags\t\t: {Fore.GREEN}{split_HL_flags[4:] + split_flags[0:]}{Style.RESET_ALL}')
     print('         |---> URG: {3}{0}{4}, ACK: {3}{1}{4}, PSH: {3}{2}{4}'
           .format(
               split_flags.bin[2], 
@@ -45,14 +45,14 @@ def getTCPHeader(tcp):
         
 def getUDPHeader(udp):
     print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}UDP Header:{Style.RESET_ALL}')
-    print(f'     - Src. Port\t\t\t: {Fore.GREEN}{udp[0]}{Style.RESET_ALL}')
-    print(f'     - Dest. Port\t\t\t: {Fore.GREEN}{udp[1]}{Style.RESET_ALL}')
+    print(f'     - Src. Port\t\t: {Fore.GREEN}{udp[0]}{Style.RESET_ALL}')
+    print(f'     - Dest. Port\t: {Fore.GREEN}{udp[1]}{Style.RESET_ALL}')
 
 def getICMPHeader(icmp):
     print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}ICMP Header:{Style.RESET_ALL}')
-    print(f'     - Type\t\t\t: {Fore.GREEN}{icmp[0]}{Style.RESET_ALL}')
-    print(f'     - Code\t\t\t: {Fore.GREEN}{icmp[1]}{Style.RESET_ALL}')
-    print(f'     - Identifier\t\t: {Fore.GREEN}{icmp[3]}{Style.RESET_ALL}')
+    print(f'     - Type\t\t: {Fore.GREEN}{icmp[0]}{Style.RESET_ALL}')
+    print(f'     - Code\t\t: {Fore.GREEN}{icmp[1]}{Style.RESET_ALL}')
+    print(f'     - Identifier\t: {Fore.GREEN}{icmp[3]}{Style.RESET_ALL}')
 
 # def getData(data):
 #     print(f'[{Fore.CYAN}!{Style.RESET_ALL}] {Fore.CYAN}Data:{Style.RESET_ALL}')
