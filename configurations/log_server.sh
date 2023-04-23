322e1836de4cb9925a107ca84c7178c9d18d7a9d
ge-wijayanto <greg.evan01@gmail.com>
2023-04-23 23:58:18 +0700

    Add send to log server script

--- configurations/log_server.sh
+++ configurations/log_server.sh
@@ -8,7 +8,7 @@ echo -e "\n${RED}NOTICE!!${NC}"
 echo -e "Sending ${CYAN}log files${NC} to ${GREEN}Log Server${NC}..."
 
 # SCP command
-# scp -i <SSH Private Key> -r /path/to/local/log/files <username>@<hostname>:/path/to/remote/log/files
+# scp -i <SSH Private Key> -r /path/to/local/log/files <username>@<Server IP/FQDN>:/path/to/remote/log/files
 yes | scp -i ssh.key -r /home/ge-wijayanto/python-sniffer/log_files/ ge-wijayanto@log-server.duckdns.org:/home/ge-wijayanto/
 
 echo -e "${CYAN}Log files${NC} have been sent to ${GREEN}Log Server${NC}.\n"
